9f7b87fc742888a3e64f173961ce55f6be0c6ab7
jcardozo-eth <jcardozo@ethz.ch>
2024-11-22 01:25:23 +0100

    Add initial Dagster project structure
    - Added Scaffolded Dagster project structure with necessary files using 'dagster project scaffold'
    - Added Configuration files and settings
    - Created 'da_pipeline' module with key components
      - `__init__.py` for module initialization
      - `assets.py` containing initial asset definitions
      - `da_pipeline_tests/` directory with initial test files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="da_pipeline",
+    packages=find_packages(exclude=["da_pipeline_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)

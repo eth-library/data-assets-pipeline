3a5681d9f02136d76e7dba1c318c0c4c81ece7f3
jcardozo-eth <jcardozo@ethz.ch>
2025-04-01 23:42:32 +0200

    Add sensors and job definitions to pipeline initialization
    This update integrates `xml_file_sensor` and `ingest_sip_job` into the `Definitions` within the pipeline initialization. It ensures the pipeline can now utilize these components for improved functionality.

--- da_pipeline/__init__.py
+++ da_pipeline/__init__.py
@@ -1,9 +1,12 @@
 from dagster import Definitions, load_assets_from_modules
 
 from . import assets
+from .sensors import xml_file_sensor, ingest_sip_job
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    jobs=[ingest_sip_job],
+    sensors=[xml_file_sensor],
 )

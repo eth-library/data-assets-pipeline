0ab97fc6202deeb00249e0944bcfe92b045552f0
jcardozo-eth <jcardozo@ethz.ch>
2025-05-13 04:05:59 +0200

    Refactor pipeline definitions into a separate module
    Moved the Dagster definitions from `__init__.py` to `definitions.py` module, as shown in Dagster project demos.

---
+++ da_pipeline/definitions.py
@@ -0,0 +1,12 @@
+from dagster import Definitions, load_assets_from_modules
+
+from da_pipeline import assets
+from da_pipeline.sensors import xml_file_sensor, ingest_sip_job
+
+all_assets = load_assets_from_modules([assets])
+
+defs = Definitions(
+    assets=all_assets,
+    jobs=[ingest_sip_job],
+    sensors=[xml_file_sensor],
+)

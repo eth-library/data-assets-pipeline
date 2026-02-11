6bb98076f313c347c4284249d333a10f4214895d
jcardozo-eth <jcardozo@ethz.ch>
2026-02-11 11:58:16 +0100

    build(cli): stop overriding version in Nix build
    Nix no longer injects a version via ldflags, so the binary uses the
    default "dev" from version.go. Release builds get the real version
    injected by GoReleaser from the git tag.

--- cli/package.nix
+++ cli/package.nix
@@ -2,13 +2,12 @@
 
 buildGoApplication {
   pname = "dap";
-  version = "0.1.0";
+  version = "dev";
   src = lib.cleanSource ./.;
   modules = ./gomod2nix.toml;
 
   ldflags = [
     "-s" "-w"
-    "-X github.com/eth-library/dap/cli/cmd.version=0.1.0"
   ];
 
   postInstall = ''

b42ab9d97de3163debc433118e049aef0279e015
jcardozo-eth <jcardozo@ethz.ch>
2026-02-18 18:10:38 +0100

    chore(cli): remove Go CLI source code
    Remove the entire Go-based CLI implementation (Cobra + Lipgloss)
    including commands, internal packages, Go module files, Nix flake,
    goreleaser config, and package.nix. The CLI is being rewritten in
    Python with Typer + Rich.

--- cli/cmd/meta/meta.go
+++
@@ -1,9 +0,0 @@
-// Package meta contains CLI maintenance commands (hidden from regular users).
-package meta
-
-import "github.com/spf13/cobra"
-
-// Commands returns hidden CLI maintenance commands.
-func Commands() []*cobra.Command {
-	return []*cobra.Command{}
-}
